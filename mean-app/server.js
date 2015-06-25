b0c32bf478cabb1475c2a68515ea709c86cf05fa
bradcedergren <b_cedergren@hotmail.com>
2015-06-25 15:31:05 -0500

    Finished Project

--- mean-app/server.js
+++ mean-app/server.js
@@ -1,13 +1,14 @@
 // BASE SETUP
 // ======================================
+
 // CALL THE PACKAGES --------------------
-var express     = require('express'); // call express
-var app         = express(); // define our app using express
-var bodyParser  = require('body-parser'); // get body-parser
-var morgan      = require('morgan'); // used to see requests
-//var mongoose    = require('mongoose'); // for working w/ our database
-var config      = require('./config');
-var path        = require('path');
+var express    = require('express');		// call express
+var app        = express(); 				// define our app using express
+var bodyParser = require('body-parser'); 	// get body-parser
+var morgan     = require('morgan'); 		// used to see requests
+var mongoose   = require('mongoose');
+var config 	   = require('./config');
+var path 	   = require('path');
 
 // APP CONFIGURATION ==================
 // ====================================
@@ -17,18 +18,17 @@ app.use(bodyParser.json());
 
 // configure our app to handle CORS requests
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-
-    next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
 });
 
-// log all requests to the console
+// log all requests to the console 
 app.use(morgan('dev'));
 
-// connect to our database
-//mongoose.connect(config.database);
+// connect to our database (hosted on modulus.io)
+mongoose.connect(config.database); 
 
 // set static files location
 // used for requests that our frontend will make
@@ -41,11 +41,11 @@ app.use(express.static(__dirname + '/public'));
 var apiRoutes = require('./app/routes/api')(app, express);
 app.use('/api', apiRoutes);
 
-// MAIN CATCHALL ROUTE ---------------
+// MAIN CATCHALL ROUTE --------------- 
 // SEND USERS TO FRONTEND ------------
 // has to be registered after API ROUTES
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 
 // START THE SERVER
